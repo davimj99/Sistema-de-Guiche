2e05f7b0d3d777326e95eec6389c0e63c18328a7
Davi Souza <davimj99@gmail.com>
2026-05-06 14:53:15 -0300

    Comentando api admin.py, para remover visibilidade do painel administrador django

--- senha_system/api/admin.py
+++ senha_system/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Senha, Guiche
+#from .models import Senha, Guiche
 
-admin.site.register(Senha)
-admin.site.register(Guiche)
+#admin.site.register(Senha)
+#admin.site.register(Guiche)
