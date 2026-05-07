e3ef82ad4865820e54a273f5cbccc147d7ffadae
Davi Souza <davimj99@gmail.com>
2026-05-07 18:42:53 -0300

    Atualizando rota env

--- senha_system/senha_system/settings.py
+++ senha_system/senha_system/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q#+qmk$19u(sd*#@2$ku%s^ldo76nm34#y-hm6t71gnw%5@aph'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
