619ba85522a2d36a2a8777afa25ebde77cc2d273
Davi Souza <davimj99@gmail.com>
2026-06-26 19:01:28 -0300

    feat(atendimentos): nova funcionalidade de tipos de atendimentos implementada com forms no painel admin

--- senha_system/atendimentos/admin.py
+++ senha_system/atendimentos/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Atendimento
+from .forms import AtendimentoForm
 
-admin.site.register(Atendimento)
+@admin.register(Atendimento)
+class AtendimentoAdmin(admin.ModelAdmin):
+    form = AtendimentoForm
--- senha_system/atendimentos/models.py
+++ senha_system/atendimentos/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 class Atendimento(models.Model):
+
+    TIPOS_ATENDIMENTO = [
+        ('matricula', 'Matrícula'),
+        ('financeiro', 'Financeiro'),
+        ('secretaria', 'Secretaria'),
+        ('declaração', 'Declaração'),
+        ('grade', 'Grade'),
+        ('suporte', 'Suporte'),
+    ]
+
     senha = models.ForeignKey("filas.Senha", on_delete=models.CASCADE)
     inicio = models.DateTimeField(auto_now_add=True)
+    tipo = models.CharField(
+        max_length=20,
+        choices=TIPOS_ATENDIMENTO,
+        null=True,
+        blank=True
+    )
     fim = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.senha} - {self.tipo}'
