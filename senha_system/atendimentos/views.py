79b75c96a653573fef5573ac2f93cb29a5a05250
Davi Souza <davimj99@gmail.com>
2026-08-18 19:47:27 -0300

    feat: adiciona filtros de data ao relatorio de atendimentos

--- senha_system/atendimentos/views.py
+++ senha_system/atendimentos/views.py
@@ -3,29 +3,25 @@ from django.http import HttpResponse
 from xhtml2pdf import pisa
 from .utils import get_atendimentos
 from django.conf import settings
-import os
-
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
+import os
 
 
 # 🔐 PERMISSÃO ADMIN
 def is_super_admin(user):
     return user.is_superuser
 
+
 @login_required
 @user_passes_test(is_super_admin)
-def relatorio_atendimentos(request):
-    return render(request, 'atendimentos/atendimento.html')
-
-
-
 def relatorio_atendimentos(request):
     atendimentos = get_atendimentos(request)
 
     return render(request, 'atendimentos/atendimentos.html', {
         'atendimentos': atendimentos
     })
+
 
 def gerar_pdf(request):
     atendimentos = get_atendimentos(request)
